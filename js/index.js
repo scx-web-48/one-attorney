3a4356d1e33e5c2b3ecb2fc4fb4257bea593bcb4
scx-web-48 <1317794151@qq.com>
2019-03-27 21:21:27 +0800

    主页轮播图

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+    // 轮播图
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });      
+})
