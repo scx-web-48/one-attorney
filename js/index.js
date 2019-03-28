c761713ca336a84b0704aff40963f67d31fa02c5
scx-web-48 <1317794151@qq.com>
2019-03-28 17:30:35 +0800

    首页大部分功能（只剩余静态）

--- js/index.js
+++ js/index.js
@@ -3,9 +3,21 @@ $(function(){
     var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         paginationClickable: true,
-        spaceBetween: 30,
         centeredSlides: true,
-        autoplay: 2500,
-        autoplayDisableOnInteraction: false
-    });      
+        autoplay: 3500, //间隔时间
+        // autoplayDisableOnInteraction: false
+    });     
+    //业务领域--主题-- 选项卡
+    $('#businessBody a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show');
+        $(this).parent().siblings().removeClass('active');
+        $(this).parent().addClass('active');
+    })
+    $('#eliteBody a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show');
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+      })
 })
