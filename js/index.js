7d4c9e268a0e886073e105d0f548968674c4d8f5
scx-web-48 <1317794151@qq.com>
2019-03-29 14:22:18 +0800

    主页--精英律师--头部切换效果

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 $(function(){
+    // $('#eliteTabBox a').width( $('#eliteBodyTit')[0].offsetWidth/4 + 'px');
+    console.log($('#eliteBodyTit'))
     // 轮播图
-    var swiper = new Swiper('.swiper-container', {
+    var swiper = new Swiper('.banner', {
         pagination: '.swiper-pagination',
         paginationClickable: true,
         centeredSlides: true,
@@ -14,10 +16,26 @@ $(function(){
         $(this).parent().siblings().removeClass('active');
         $(this).parent().addClass('active');
     })
+    //精英律师--主题-- 选项卡
     $('#eliteBody a').click(function (e) {
         e.preventDefault()
         $(this).tab('show');
         $(this).siblings().removeClass('active');
         $(this).addClass('active');
-      })
+    })
+    //精英律师--主题-- 箭头
+    $('#nextEliteTab').click(function(){
+       var eliteTabBoxLeft = $('#eliteTabBox').scrollLeft();
+       console.log($('#eliteTabBox'))
+    })
+    $('#eliteTabBox').scroll(function(){
+        console.log(e.offsetLeft)
+    })
+    new Swiper('.eliteBodyTit', {
+        pagination: null,
+        slidesPerView: 4,
+        paginationClickable: true,
+        spaceBetween: 0,
+        nextButton: '.swiper-button-next',
+    });
 })
