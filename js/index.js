cc1ada1bf4843519cfc6a39a00c1eaef689f0303
scx-web-48 <1317794151@qq.com>
2019-04-02 15:28:45 +0800

    专题页-1-初次

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 $(function(){
     // $('#eliteTabBox a').width( $('#eliteBodyTit')[0].offsetWidth/4 + 'px');
-    console.log($('#eliteBodyTit'))
+    
     // 轮播图
     var swiper = new Swiper('.banner', {
         pagination: '.swiper-pagination',
         paginationClickable: true,
         centeredSlides: true,
         autoplay: 3500, //间隔时间
-        // autoplayDisableOnInteraction: false
+        autoplayDisableOnInteraction: false //手动控制之后轮播图依旧生效
     });     
     //业务领域--主题-- 选项卡
     $('#businessBody a').click(function (e) {
@@ -24,13 +24,6 @@ $(function(){
         $(this).addClass('active');
     })
     //精英律师--主题-- 箭头
-    $('#nextEliteTab').click(function(){
-       var eliteTabBoxLeft = $('#eliteTabBox').scrollLeft();
-       console.log($('#eliteTabBox'))
-    })
-    $('#eliteTabBox').scroll(function(){
-        console.log(e.offsetLeft)
-    })
     new Swiper('.eliteBodyTit', {
         pagination: null,
         slidesPerView: 4,
---
+++ js/special-one.js
@@ -0,0 +1,25 @@
+$(function () {
+  // 轮播图
+  var swiper = new Swiper('.banner', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    centeredSlides: true,
+    autoplay: 3500, //间隔时间
+    autoplayDisableOnInteraction: false
+  });
+  //精英律师--主题-- 选项卡
+  $('#eliteBody a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show');
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+})
+  //精英律师--主题-- 箭头
+  new Swiper('.eliteBodyTit', {
+    pagination: null,
+    slidesPerView: 4,
+    paginationClickable: true,
+    spaceBetween: 0,
+    nextButton: '.swiper-button-next',
+  });
+})
